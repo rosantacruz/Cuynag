46d1d3da02d0709c64b4bf34899f93943729e992
rosantacruz <rodrisantacruz92@hotmail.com>
2021-12-27 19:26:23 +0100

    1er clase JS

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Hola! Ingrese su nombre por favor");
+let saludo = console.log("¿Como estas " + nombre + "?")
+let edad = prompt("¿Podrias indicarme tu edad?")
+let diferencia = parseInt(edad - 29)
+let alerta = alert("Eso significa que tenemos " + diferencia + " años de diferencia")
+ 
