dea59bebf43058fa83dd71f169b5538bc0b8495f
rosantacruz <rodrisantacruz92@hotmail.com>
2021-12-29 17:55:54 +0100

    Desafio 2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
 
-/*
-Clase 1
-
-let nombre = prompt("Hola! Ingrese su nombre por favor");
-let saludo = console.log("¿Como estas " + nombre + "?")
-let edad = prompt("¿Podrias indicarme tu edad?")
-let diferencia = parseInt(edad - 29)
-let alerta = alert("Eso significa que tenemos " + diferencia + " años de diferencia")
-*/
-
-/*  Clase 2 */
-
 let temperatura = parseInt(prompt("Ingresa la temperatura actual: "));
 
 let clima = prompt("Ingrese el clima actual - lluvia/soleado: ");
