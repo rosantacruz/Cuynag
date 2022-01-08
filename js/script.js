dfa0b650715988d4fc399ce7f7b1d6d70439b4d8
rosantacruz <rodrisantacruz92@hotmail.com>
2022-01-08 21:22:47 +0100

    Desafio 3

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
+let nombre = prompt("Ingresar un nombre de un tutor:")
 
-let temperatura = parseInt(prompt("Ingresa la temperatura actual: "));
-
-let clima = prompt("Ingrese el clima actual - lluvia/soleado: ");
-
-
-if ((temperatura <= 5) && (clima == "lluvia")) {
-    alert ("Se recomienda utilizar una chaqueta de abrigo e impermeable")
-}   else if ((temperatura <= 5) && (clima == "soleado")) {
-    alert ("Se recomienda utilizaar una chaqueta de abrigo")
-}   else if (((temperatura >= 6) && (temperatura <= 15)) && (clima == "lluvia")) {
-    alert("Se recomienda utilizar una chaqueta ligera e impermeable")
-}   else if (((temperatura >= 6) && (temperatura <= 15)) && (clima == "soleado")) {
-    alert("Se recomienda utulizar una chaqueta ligera")
-}   else if ((temperatura >= 16) && (clima == "lluvia")) {
-    alert("Se recomienda utilizar un paragua")
-}   else if (( temperatura = " ") || (clima=" ")) {
-    alert("Valores incorrectos")
-}   else {
-    alert("No se recomienda usar nada")
+while (nombre != "florencia") {
+    switch (nombre) {
+        case "milton":
+            alert("Milton es un tutor");
+            break;
+        case "rodrigo":
+            alert("Rodrigo es un tutor");
+            break;
+        case "juan":
+            alert("Juan es un tutor");
+            break;
+        case "franco":
+            alert("Franco es un tutor");
+            break;
+        case "gonzalo":
+            alert("Gonzalo es un tutor");
+            break;
+        case "luis":
+            alert("Luis es un tutor");
+            break;
+        default:
+            alert("Nombre incorrecto")
+            break;
+    }
+    nombre =prompt("Ingresar un nombre de un tutor:")
+    
 }
+if (nombre = "florencia")
+        alert("Florencia es la profesora");
+
+
