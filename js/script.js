f45e8160180579f66300e8e0addfa082213ea96f
rosantacruz <rodrisantacruz92@hotmail.com>
2022-01-17 22:04:29 +0100

    Desafio 5

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
+class Servicio {
+    constructor(tipo, modalidad, precio, ) {
+        this.tipo = tipo;
+        this.modalidad = modalidad;
+        this.precio = parseFloat(precio);
+    }
+    precioPyme() {
+        this.precio = this.precio * 1.25;
+        console.log("El precio es: " + this.precio);
+    }
 
-function multiplicar(numero1,numero2) {
-    return numero1 * numero2;
-}
-function dividir(numero1,numero2) {
-    return numero1 / numero2
-}
-
-var eleccion = prompt("Introduzca el servicio buscado: asesoramiento/consultoria");
-var empresa = prompt("Introduzca el tamaño de su empresa: pyme/gran");
-
-if ((eleccion == "asesoramiento") && (empresa == "pyme")) {
-    var resultado = multiplicar(150000,1.25);
-    alert("El costo del servicio es: " + resultado + " pesos");
-}
-if ((eleccion == "asesoramiento") && (empresa == "gran")) {
-    var resultado = multiplicar(150000,1.5);
-    alert("El costo del servicio es: " + resultado + " pesos");
-}
-
-if ((eleccion == "consultoria") && (empresa == "pyme")) {
-    var resultado = dividir(150000,2);
-    alert("El costo del servicio es: " + resultado + " pesos");
-}
-if ((eleccion == "consultoria") && (empresa == "gran")) {
-    var resultado = dividir(150000,2);
-    alert("El costo del servicio es: " + resultado + " pesos");
+    precioEmpresa() {
+        this.precio = this.precio * 1.5;
+        console.log("El precio es: " + this.precio);
+    }
+    modDigital() {
+        if (this.modalidad.toLowerCase() == "digital") {
+            this.precio * 1.5;
+            console.log("El precio es: " + this.precio);
+        }
+    }
+    modPresencial() {
+        if (this.modalidad.toLowerCase() == "presencial") {
+            this.precio * 2;
+            console.log("El precio es: " + this.precio);
+        }
+    }
 }
 
+
+const asesoramiento1 = new Servicio("asesoramiento", "", 150000);
+
+asesoramiento1.precioPyme();
+asesoramiento1.precioEmpresa();
+asesoramiento1.modPresencial();
+asesoramiento1.modalidad = prompt("Ingrese la modalidad del servicio digital/presencial: ")
